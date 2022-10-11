784f444ebefc60d86c407b79c3a611f4f390c16e
ZeDmEv <dmirtii.zero@yandex.ru>
2022-10-11 12:41:01 +0300

    tests passed

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,11 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
